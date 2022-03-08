88cbdec2612e98b1de4c7aa654685fcb3b95a466
sapir0507 <sapir.s@info.cts.org.il>
2022-03-09 00:53:40 +0200

    commit "routing"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { RegisterModule } from './../layout/register/register.module';
+import { LoginModule } from './../layout/login/login.module';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app.app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    LoginModule,
+    RegisterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
